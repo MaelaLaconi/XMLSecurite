61ada10a99165651ef06c8d23477f913470cb244
MaelaLaconi <44729941+MaelaLaconi@users.noreply.github.com>
2021-04-16 11:42:54 +0200

    db connection

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 package com.company;
 
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 	// write your code here
         System.out.println("Hello world");
 
@@ -12,7 +13,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         /***************************************************/
-        while(!selection.equals("Q")){
+        /*while(!selection.equals("Q")){
             System.out.println("Veuillez faire votre choix");
             System.out.println("-------------------------\n");
             System.out.println("(R)echercher");
@@ -24,6 +25,19 @@ public class Main {
             selection = input.next();
 
             System.out.println("Votre choix est : " + selection);
-        }
+        }*/
+        Class.forName("com.mysql.jdbc.Driver");
+
+        String DBurl = "jdbc:mysql://localhost:3306/restaurant";
+        String username = "root" ;
+        String password = "" ;
+
+        Connection connection = DriverManager.getConnection(DBurl, username, password) ;
+
+
+        String sql = "INSERT INTO `contient` (`numcom`, `numplat`, `quantite`) VALUES ('10', '11', '12');" ;
+        Statement statement = connection.createStatement();
+
+        int rows = statement.executeUpdate(sql) ;
     }
 }
